c67757dd1efd9f48da9990f061430c7657724519
kimtee92 <kimtee92@gmail.com>
2018-09-05 17:32:28 +1000

    cors

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+//put headers for cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // use JWT auth to secure the api
 app.use(jwt());
 
