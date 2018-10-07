18a33bf00455caec70e3d5432fc6c5355f66f955
kimtee92 <kimtee92@gmail.com>
2018-10-07 22:43:31 +1100

    no cors test

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //put headers for cors
-app.use(function (req, res, next) {
-  var allowedOrigins = ['https://tvt-driver.herokuapp.com', 'https://tvt-enforcer.herokuapp.com'];
-  var origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
+// app.use(function (req, res, next) {
+//   var allowedOrigins = ['https://tvt-driver.herokuapp.com', 'https://tvt-enforcer.herokuapp.com'];
+//   var origin = req.headers.origin;
+//   if (allowedOrigins.indexOf(origin) > -1) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+//   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   return next();
+// });
 
 // use JWT auth to secure the api
 app.use(jwt());
