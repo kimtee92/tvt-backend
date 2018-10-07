3fff5d8f292173ced2e2fc7c944acebabe60adf3
kimtee92 <kimtee92@gmail.com>
2018-10-07 23:10:33 +1100

    remove headers totally

--- server.js
+++ server.js
@@ -10,19 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-//put headers for cors
-app.use(function (req, res, next) {
-  var allowedOrigins = ['https://tvt-driver.herokuapp.com', 'https://tvt-enforcer.herokuapp.com'];
-  var origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  //res.header("Access-Control-Allow-Origin", "*");
-  //res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // use JWT auth to secure the api
 app.use(jwt());
 
