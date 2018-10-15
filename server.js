041fa9b01a078ef553413d98eadff77861006a87
kimtee92 <kimtee92@gmail.com>
2018-10-16 00:45:56 +1100

    compression

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 ﻿require('rootpath')();
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
+
+// compress 
+app.use(compression())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
